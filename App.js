a55f92dbbfc8b85448ea8cdd57d57e01954b18a7
HugoTse <hugotse8@gmail.com>
2020-08-30 22:22:54 +0800

    Adding React files for the Afrihack Hackathon

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header.js';
+import Form from './components/Form.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Component} from 'react';
+import './Header.css';
+
+class Header extends Component{
+    render(){
+        return (
+            <div id="header">
+                <h1>2020 August Afrihack</h1>
+            </div>
+        );
+    }
+}
+export default Header;
